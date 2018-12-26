f70e9319722118ead661d81bf59f263653abd8df
章扬 <foreveryounix@gmail.com>
2018-12-26 17:50:27 +0800

    实现AsyncTask工具类方法. 在后台完成 FlickrFetchr()

--- app/src/main/java/com/iyounix/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/iyounix/photogallery/PhotoGalleryFragment.java
@@ -1,16 +1,21 @@
 package com.iyounix.photogallery;
 
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
+
 public class PhotoGalleryFragment extends Fragment {
 
     private RecyclerView mPhotoRecyclerView;
+    private static final String TAG = "PhotoGalleryFragment";
 
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
@@ -20,6 +25,8 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        //调用execute()方法会启动AsyncTask，进而触发后台线程并调用doInBackground(...)方法
+        new FetchItemsTask().execute();
     }
 
     @Override
@@ -30,5 +37,21 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
 
         return v;
+    }
+
+    //实现AsyncTask工具类方法
+    private class FetchItemsTask extends AsyncTask<Void,Void,Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                String result = new FlickrFetchr()
+                        .getUrlString("https://iyounix.com");
+                Log.i(TAG, "doInBackground: Fetched contents of URL: " + result);
+            } catch (IOException e) {
+                Log.e(TAG, "doInBackground: Failed to fetch URL: ", e );
+            }
+            return null;
+        }
     }
 }
