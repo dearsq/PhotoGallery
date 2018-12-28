318ebd2261cffb9113d18fcc4775b41792d4bafd
章扬 <foreveryounix@gmail.com>
2018-12-28 14:12:28 +0800

    取出每张图片的信息，生成一个个GalleryItem对象，再将它们添加到List中

--- app/src/main/java/com/iyounix/photogallery/FlickrFetchr.java
+++ app/src/main/java/com/iyounix/photogallery/FlickrFetchr.java
@@ -3,11 +3,16 @@ package com.iyounix.photogallery;
 import android.net.Uri;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 public class FlickrFetchr {
 
@@ -61,8 +66,29 @@ public class FlickrFetchr {
                     .build().toString();
             String jsonString = getUrlString(url);
             Log.i(TAG, "Received JSON:" + jsonString);
-        } catch (IOException e) {
-            Log.e(TAG, "Failed to fetch items", e);
+            JSONObject jsonBody = new JSONObject(jsonString);
+        } catch (IOException ioe) {
+            Log.e(TAG, "Failed to fetch items", ioe);
+        } catch (JSONException je) {
+            Log.e(TAG, "Failed to parse JSON", je);
+        }
+    }
+
+    //取出每张图片的信息，生成一个个GalleryItem对象，再将它们添加到List中
+    private void parseItems(List<GalleryItem> items, JSONObject jsonBody)
+            throws IOException, JSONException {
+        JSONObject photosJsonObject = jsonBody.getJSONObject("photos");
+        JSONArray photoJsonArray = photosJsonObject.getJSONArray("photo");
+        for (int i = 0; i < photoJsonArray.length(); i++) {
+            JSONObject photoJsonObject = photoJsonArray.getJSONObject(i);
+            GalleryItem item = new GalleryItem();
+            item.setId(photoJsonObject.getString("id"));
+            item.setCaption(photoJsonObject.getString("title"));
+            if (!photoJsonObject.has("url_s")) {
+                continue;
+            }
+            item.setUrl(photoJsonObject.getString("url_s"));
+            items.add(item);
         }
     }
 }
